c5ea10e03d7574b4588cd05e30f33c8eb992126f
jianhui.xu <jianhui.xu@aihuishou.com>
2018-12-13 16:56:07 +0800

    init

--- lib/widgets/FlutterRow.dart
+++ lib/widgets/FlutterRow.dart
@@ -4,13 +4,9 @@ class FlutterRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Expanded(child: Container(color: Colors.brown,),flex: 1,),
-          Expanded(child: Container(color: Colors.blue,), flex: 1,),
-          Container(color: Colors.amberAccent,constraints: BoxConstraints(minHeight: 100, maxHeight: 100),)//未参与Expanded，
-          // 除了此Widget的空余区域均留给Expanded修饰的Colors.brown & Colors.blue,进行1：1等分
-        ],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[Text("One"), Text("Two"), Text("Three")],
       ),
     );
   }
