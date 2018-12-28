af9283836646dad5b4fe2b21381fad3e398137cd
jianhui.xu <jianhui.xu@aihuishou.com>
2018-12-28 15:50:11 +0800

    ImageWidget

--- lib/widgets/FlutterImage.dart
+++ lib/widgets/FlutterImage.dart
@@ -6,12 +6,23 @@ class FlutterImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Image.network(
-      "http://img2018.cnblogs.com/news/66372/201809/66372-20180921155512352-228425089.jpg",
-      color: Colors.blue,
-      colorBlendMode: BlendMode.saturation,
+      "https://upload.jianshu.io/users/upload_avatars/3884536/d847a50f1da0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240",
+          width: 400,
+          height: 400,
+          fit: BoxFit.contain,
+          centerSlice: Rect.fromLTWH(10, 10, 10, 10),
     ));
   }
 }
 
 //显示网络图片：Image.network("https://upload.jianshu.io/users/upload_avatars/3884536/d847a50f1da0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240"),
 //加载File图片： Image.file(File("/sdcard/flutter.jpeg")), 注意在AndroidManifest.xml中配置读写文件权限。
+
+//centerSlice//////////////
+//Image.network(
+//      "https://upload.jianshu.io/users/upload_avatars/3884536/d847a50f1da0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240",
+//          width: 400,
+//          height: 400,
+//          fit: BoxFit.contain,
+//          centerSlice: Rect.fromLTWH(10, 10, 10, 10),
+//    )
