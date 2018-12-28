ccc6620a6c7a93db6b67237652a06e26615a110b
jianhui.xu <jianhui.xu@aihuishou.com>
2018-12-28 11:13:17 +0800

    ImageWidget

--- lib/widgets/FlutterImage.dart
+++ lib/widgets/FlutterImage.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 
 class FlutterImage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset("assets/images/flutter.jpeg")
-    );
+        child: Image.network(
+      "http://img2018.cnblogs.com/news/66372/201809/66372-20180921155512352-228425089.jpg",
+      color: Colors.blue,
+      colorBlendMode: BlendMode.saturation,
+    ));
   }
-
 }
 
 //显示网络图片：Image.network("https://upload.jianshu.io/users/upload_avatars/3884536/d847a50f1da0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240"),
