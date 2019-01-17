d9d64a941fffd4d4213ac880840da6fb46130f31
jianhui.xu <jianhui.xu@aihuishou.com>
2019-01-17 11:45:40 +0800

    FlutterGridView

--- lib/widgets/FlutterGridView.dart
+++ lib/widgets/FlutterGridView.dart
@@ -1,28 +1,64 @@
 import 'package:flutter/material.dart';
 
 class FlutterGridView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisSpacing: 10.0,
-      mainAxisSpacing: 10.0,
-      padding: EdgeInsets.all(10.0),
-      crossAxisCount: 3,
-      children: getWidgetList(),
-    );
-  }
 
-  List<Widget> getWidgetList() {
+
+
+
+  //GridView写法二
+
+//  @override
+//  Widget build(BuildContext context) {
+//    List<String> datas = getDataList();
+//    return GridView.builder(
+//        itemCount: datas.length,
+//        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//          //横轴元素个数
+//            crossAxisCount: 3,
+//            //纵轴间距
+//            mainAxisSpacing: 20.0,
+//            //横轴间距
+//            crossAxisSpacing: 10.0,
+//            //子组件宽高长度比例
+//            childAspectRatio: 1.0),
+//        itemBuilder: (BuildContext context, int index) {
+//          //Widget Function(BuildContext context, int index)
+//          return getItemContainer(datas[index]);
+//        });
+//  }
+
+  //GridView写法一
+
+//  @override
+//  Widget build(BuildContext context) {
+//    return GridView.count(
+//      crossAxisSpacing: 10.0,
+//      mainAxisSpacing: 10.0,
+//      padding: EdgeInsets.all(10.0),
+//      crossAxisCount: 3,
+//      children: getWidgetList(),
+//    );
+//  }
+//
+
+  List<String> getDataList() {
     List<String> list = [];
     for (int i = 0; i < 100; i++) {
       list.add(i.toString());
     }
+    return list;
+  }
 
-    return list
-        .map((item) => Container(
-              child: Text(item),
-              color: Colors.blue,
-            ))
+  List<Widget> getWidgetList() {
+    return getDataList()
+        .map((item) =>getItemContainer(item))
         .toList();
+  }
+
+  getItemContainer(String item) {
+    return Container(
+      child: Text(item),
+      color: Colors.blue,
+    );
   }
 }
